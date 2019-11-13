531f31f45ec005a5690bc7f4a401dd2e7cad2bc3
Dariusz Okonski <darek200180@gmail.com>
2019-11-13 13:01:45 +0000

    initial

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
+
+// "https://jsonplaceholder.typicode.com/users"
+// https://robohash.org/${props.monster.id}?set=set2&size=180x180
