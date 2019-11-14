0b2c1fcc6430ffd7628c22ed301f7245ed305f18
Dariusz Okonski <darek200180@gmail.com>
2019-11-14 10:39:58 +0000

    add propTypes and handleChange method

--- src/components/FindMonster/FindMonster.jsx
+++ src/components/FindMonster/FindMonster.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import styles from './FindMonster.module.css';
+import PropTypes from 'prop-types';
 
 class FindMonster extends Component {
+  handleChange = (e) => {
+    this.props.handleFind(e.target.value)
+  }
+
   render() {
     return (
       <div className={styles.container}>
         <input
+          onChange={this.handleChange}
           className={styles.input}
           type="text"
           placeholder='Search for monsters...'
@@ -13,6 +19,10 @@ class FindMonster extends Component {
       </div>
     );
   }
+}
+
+FindMonster.propTypes = {
+  handleFind: PropTypes.func.isRequired,
 }
 
 export default FindMonster;
